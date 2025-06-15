d03e484e10a38488387463c6663283f6cde37733
ndjo-byte <jonesn.96@gmail.com>
2025-06-15 15:00:25 +0200

    save_to_text() method added to Pipeline class for .txt

--- agents/pipeline.py
+++ agents/pipeline.py
@@ -2,6 +2,7 @@ from classify_agent import ClassifyAgent
 from search_agent import SearchAgent
 from calculate_agent import CalculateAgent
 import pandas as pd
+import os
 
 # Load CSV Data to DataFrame
 simple_products = pd.read_csv("../csv_files/simple_products.csv")
@@ -16,8 +17,18 @@ class ProductPipeline:
         clfy_result = self.clfy_agent.classify()
         srch_result = self.srch_agent.search()
         calc_result = CalculateAgent(srch_result, self.clfy_agent.get_product_quantities(clfy_result))
-        return calc_result.calculate()
+        self.ticket_text = calc_result.calculate()
+        return self.ticket_text
+    
+    def save_to_txt(self, filename="ticket.txt", folder="../txt"):
+        os.makedirs(folder, exist_ok=True)
+        file_path = os.path.join(folder, filename)
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write(self.ticket_text)
+        return file_path
+
 
 if __name__ == "__main__":
     pipeline = ProductPipeline("Voy a preparar una ensalada griega y necesito limpiar la cocina.")
     print(pipeline.run())
+    pipeline.save_to_txt()
